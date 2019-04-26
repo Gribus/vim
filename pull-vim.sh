929de41dea569153b7335dd198abcc857a78bdea
Alex Kehl <alexander.kehl@apprologic.de>
2019-04-26 09:57:25 +0200

    Update vim

--- pull-vim.sh
+++ pull-vim.sh
@@ -3,4 +3,4 @@ cd ~/vim
 git pull
 cp .vimrc ~/.vimrc
 cp .zshrc ~/.zshrc
-cp javascript.snippets ~/.vim/bundle/vim-snippets/javascript/snippets/javascript/
+cp javascript.snippets ~/.vim/bundle/vim-snippets/snippets/javascript/
