b8a7556c7f94d853e637c90a81beaeeb04f6d406
Alex Kehl <alexander.kehl@apprologic.de>
2019-05-13 17:24:23 +0200

    Update vim

--- pull-vim.sh
+++ pull-vim.sh
@@ -4,3 +4,5 @@ git pull
 cp .vimrc ~/.vimrc
 cp .zshrc ~/.zshrc
 cp javascript.snippets ~/.vim/bundle/vim-snippets/snippets/javascript/
+cp rc.conf ~/.config/ranger/
+cp rifle.conf ~/.config/ranger/
--- update-vim.sh
+++ update-vim.sh
@@ -3,6 +3,8 @@ cd ~/vim
 cp ~/.vimrc ~/vim/
 cp ~/.zshrc ~/vim/
 cp ~/.vim/bundle/vim-snippets/snippets/javascript/javascript.snippets ~/vim/
+cp ~/.config/ranger/rc.conf ~/vim/
+cp ~/.config/ranger/rifle.conf ~/vim/
 git add .vimrc
 git add .zshrc
 git add javascript.snippets
