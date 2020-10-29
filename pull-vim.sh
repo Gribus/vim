3262cca1ebdc522951f8881952aee303d18d88c7
Alex Kehl <alexander.kehl@apprologic.de>
2020-10-29 15:25:01 +0100

    Update vim

--- pull-vim.sh
+++ pull-vim.sh
@@ -4,6 +4,7 @@ git pull
 cp .vimrc ~/.vimrc
 cp .zshrc ~/.zshrc
 cp .tigrc ~/.tigrc
+cp .tmux.conf ~/
 cp javascript.snippets ~/.vim/bundle/vim-snippets/snippets/javascript/
 cp rc.conf ~/.config/ranger/
 cp rifle.conf ~/.config/ranger/
